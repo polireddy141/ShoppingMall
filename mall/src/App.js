1a7ecd28371db44fb2b3915f21fd5a04b702ba86
polireddy141 <polireddy.veera@gmail.com>
2025-03-05 10:53:02 +0530

    Adding task

---
+++ mall/src/App.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+import './App.css'
+import MainPage from './ShoppingFolder/Pages/MainPage'
+
+const App = () => {
+  return (
+    <div>
+      
+      <MainPage/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ mall/src/ShoppingFolder/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footerSection">
+      
+        <div className="bannerBox">
+            <img src="assets/footer.png" alt="banner" />
+        </div>
+    
+      </div>
+    </div>
+  )
+}
+
+export default Footer
